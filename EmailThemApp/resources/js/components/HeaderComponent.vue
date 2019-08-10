55eab5a920df974f92f9e10117ea5a94f4090b77
Butler Fuqua <butlerfuqua@gmail.com>
2019-08-10 16:26:39 -0400

    Add initial sidebar and page  styles, and add vue header component.

---
+++ EmailThemApp/resources/js/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <div id="menuBtn">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
