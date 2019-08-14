546a7bc78d5ddc2d98c769a933a42802e6322f1a
Butler Fuqua <butlerfuqua@gmail.com>
2019-08-13 20:39:14 -0400

    Add initial styles for signups landing page.

--- EmailThemApp/resources/views/signups/create.blade.php
+++ EmailThemApp/resources/views/signups/create.blade.php
@@ -4,16 +4,28 @@
 @section('body')
 
 
-<div id="app" class="container mt-3">
+<div id="app" class="signupContainer">
 
-    <form action="/signups" method="post">
-        @csrf
-        <div class="form-group">
-            <label for="email">Enter Email</label>
-            <input id="email" name="email" type="email" class="form-control" required>
-            <input type="submit" class="btn btn-outline-success mt-3">
-        </div>
-    </form>
+<div class="leftSide">
+
+</div>
+<div class="rightSide">
+    <div class="theContent shadow-lg">
+        <form action="/signups" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="email">Enter Email</label>
+                <input id="email" name="email" type="email" class="form-control" required>
+                <input type="submit" class="button button-1 button-2 mt-3">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+
+
 </div>
 
 
@@ -21,3 +33,18 @@
 
 
 @endsection
+
+
+
+{{-- 
+
+<form action="/signups" method="post">
+    @csrf
+    <div class="form-group">
+        <label for="email">Enter Email</label>
+        <input id="email" name="email" type="email" class="form-control" required>
+        <input type="submit" class="btn btn-outline-success mt-3">
+    </div>
+</form> 
+
+--}}
